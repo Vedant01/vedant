732e0b30780564bd40d6d54b00d309ba58608b0e
Vedant01 <vedant.agarwal312@gmail.com>
2025-04-24 04:29:20 +0530

    fix: use CommonJS import for react-dom/server

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,5 +1,6 @@
-import { renderToReadableStream } from 'react-dom/server';
+import pkg from 'react-dom/server';
 import { RemixServer } from '@remix-run/react';
+const { renderToReadableStream } = pkg;
 
 export default function handleRequest(
   request,
